42d9b3a135bc9d2ad4c975d335c0b0948d45f392
Coelho <guiotako2442@gmail.com>
2025-12-18 17:49:59 -0300

    Create 01_schema.sql

---
+++ scripts/scripts/01_schema.sql
@@ -0,0 +1,38 @@
+-- Criação do Banco de Dados para a Cafeteria
+CREATE TABLE categorias (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE produtos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    categoria_id INT,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
+
+CREATE TABLE funcionarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    cargo VARCHAR(50),
+    data_admissao DATE
+);
+
+CREATE TABLE pedidos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    data_pedido DATETIME DEFAULT CURRENT_TIMESTAMP,
+    funcionario_id INT,
+    total_pedido DECIMAL(10, 2) DEFAULT 0.00,
+    FOREIGN KEY (funcionario_id) REFERENCES funcionarios(id)
+);
+
+CREATE TABLE itens_pedido (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    pedido_id INT,
+    produto_id INT,
+    quantidade INT NOT NULL,
+    subtotal DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
+    FOREIGN KEY (produto_id) REFERENCES produtos(id)
+);
