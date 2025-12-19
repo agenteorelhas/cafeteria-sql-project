34fb0b97c7cc5a4fcd753410f0baace7f178d9ec
Coelho <guiotako2442@gmail.com>
2025-12-19 09:05:11 -0300

    Update 02_seed.sql

--- scripts/scripts/02_seed.sql
+++ scripts/scripts/02_seed.sql
@@ -24,3 +24,18 @@ INSERT INTO itens_pedido (pedido_id, produto_id, quantidade, subtotal) VALUES
 (1, 2, 1, 9.00), -- 1 Capuccino no pedido 1
 (2, 3, 1, 15.00),-- 1 Frappuccino no pedido 2
 (3, 6, 2, 24.00);-- 2 Bolos no pedido 3
+
+-- Inserindo Clientes
+INSERT INTO clientes (nome, email) VALUES ('José Roberto', 'jose@email.com'), ('Maria Clara', 'maria@email.com');
+
+-- Inserindo Ingredientes
+INSERT INTO ingredientes (nome, quantidade_estoque, unidade_medida) VALUES 
+('Grão de Café Arábica', 5000.00, 'g'),
+('Leite Integral', 10000.00, 'ml'),
+('Cacau em Pó', 1000.00, 'g');
+
+-- Definindo a Ficha Técnica (Ex: O Capuccino usa Café e Leite)
+-- Supondo que o produto 2 é o Capuccino
+INSERT INTO ficha_tecnica (produto_id, ingrediente_id, quantidade_usada) VALUES 
+(2, 1, 10.00), -- 10g de café
+(2, 2, 150.00); -- 150ml de leite
